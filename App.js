9bbde23fc16c432da217e8710b99427adcae25eb
Karma Drukpa <karmadrukpa@Karmas-MacBook-Pro.local>
2020-08-16 23:11:03 -0400

    Added basic layout for the todo list app

--- App.js
+++ App.js
@@ -1,26 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, TextInput, View, Button } from "react-native";
 
 export default function App() {
-	const [outputText, setOutputText] = useState('Karma');
-	return (
-		<View style={styles.container}>
-			<Text>{outputText}</Text>
-			<StatusBar style='auto' />
-			<Button
-				title='Change Text'
-				onPress={() => setOutputText('The text changed!')}
-			/>
-		</View>
-	);
+  return (
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder='Course Goal'
+          style={{
+            width: "80%",
+            borderColor: "black",
+            borderWidth: 1,
+            padding: 10,
+          }}
+        />
+        <Button title='ADD' />
+      </View>
+      <View></View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
+const styles = StyleSheet.create({});
